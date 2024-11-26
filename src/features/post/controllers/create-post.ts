a978f2c7c90e4c73e164f124c5c41f5f5d41d8df
chan <pavanchandupatla24@gmail.com>
2024-11-27 00:59:51 +0530

    feat: added video-upload features

--- src/features/post/controllers/create-post.ts
+++ src/features/post/controllers/create-post.ts
@@ -1,8 +1,8 @@
 import { joiValidation } from '@global/decorators/joi-validation.decorators';
-import { uploads } from '@global/helpers/cloudinary-upload';
+import { uploads, videoUpload } from '@global/helpers/cloudinary-upload';
 import { BadRequestError } from '@global/helpers/error-handler';
 import { IPostDocument } from '@post/interfaces/post.interface';
-import { postSchema, postWithImageSchema } from '@post/schemes/post.schemes';
+import { postSchema, postWithImageSchema, postWithVideoSchema } from '@post/schemes/post.schemes';
 import { imageQueue } from '@service/queues/image.queue';
 import { postQueue } from '@service/queues/post.queue';
 import { PostCache } from '@service/redis/post.cache';
@@ -35,6 +35,8 @@ export class Create {
       commentsCount: 0,
       imgVersion: '',
       imgId: '',
+      videoId: '',
+      videoVersion: '',
       createdAt: new Date(),
       reactions: { like: 0, love: 0, happy: 0, sad: 0, wow: 0, angry: 0 }
     } as IPostDocument;
@@ -76,6 +78,8 @@ export class Create {
       privacy,
       gifUrl,
       commentsCount: 0,
+      videoId: '',
+      videoVersion: '',
       imgVersion: result.version.toString(),
       imgId: result.public_id,
       createdAt: new Date(),
@@ -100,5 +104,51 @@ export class Create {
     });
 
     res.status(HTTP_STATUS.CREATED).json({ message: 'Post created with image successfully' });
+  }
+
+  @joiValidation(postWithVideoSchema)
+  public async postWithVideo(req: Request, res: Response): Promise<void> {
+    const { post, bgColor, privacy, gifUrl, profilePicture, feelings, video } = req.body;
+
+    const result: UploadApiResponse = (await videoUpload(video)) as UploadApiResponse;
+    if (!result?.public_id) {
+      throw new BadRequestError(result.message);
+    }
+
+    const postObjectId: ObjectId = new ObjectId();
+    const createdPost: IPostDocument = {
+      _id: postObjectId,
+      userId: req.currentUser!.userId,
+      username: req.currentUser!.username,
+      email: req.currentUser!.email,
+      avatarColor: req.currentUser!.avatarColor,
+      profilePicture,
+      post,
+      bgColor,
+      feelings,
+      privacy,
+      gifUrl,
+      commentsCount: 0,
+      imgVersion: '',
+      imgId: '',
+      videoVersion: result.version.toString(),
+      videoId: result.public_id,
+      createdAt: new Date(),
+      reactions: { like: 0, love: 0, happy: 0, sad: 0, wow: 0, angry: 0 }
+    } as IPostDocument;
+
+    socketIOPostObject.emit('add post', createdPost); // user will get socket data response before storing in the cache
+
+    await postCache.savePostToCache({
+      key: postObjectId,
+      currentUserId: `${req.currentUser!.userId}`,
+      uId: `${req.currentUser!.uId}`,
+      createdPost
+    });
+
+    postQueue.addPostJob('addPostToDB', { key: req.currentUser!.userId, value: createdPost });
+    //removing the image queue as we are not creating any special verison of the images in the mongodb collection so not required -> maybe in future
+
+    res.status(HTTP_STATUS.CREATED).json({ message: 'Post created with video successfully' });
   }
 }
--- src/features/post/interfaces/post.interface.ts
+++ src/features/post/interfaces/post.interface.ts
@@ -25,6 +25,8 @@ export interface IPostDocument extends Document {
   commentsCount: number;
   imgVersion?: string;
   imgId?: string;
+  videoId?: string;
+  videoVersion?: string;
   feelings?: string;
   gifUrl?: string;
   privacy?: string;
@@ -37,6 +39,7 @@ export interface IGetPostsQuery {
   username?: string;
   imgId?: string;
   gifUrl?: string;
+  videoId?: string;
 }
 
 export interface ISavePostToCache {
